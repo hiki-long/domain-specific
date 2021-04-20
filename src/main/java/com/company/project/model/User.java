cef3fb8ef573c9a7de7602e11f6f3f197f4b6d9f
Vergil <458361036@qq.com>
2021-04-20 15:39:12 +0800

    edit mapper as user

--- src/main/java/com/company/project/model/User.java
+++ src/main/java/com/company/project/model/User.java
@@ -1,12 +1,85 @@
 package com.company.project.model;
 
-import java.util.Date;
 import javax.persistence.*;
 
 public class User {
     private String uuid;
+
     private String email;
+
     private String username;
+
     private String role;
+
     private String avatar;
+
+    /**
+     * @return uuid
+     */
+    public String getUuid() {
+        return uuid;
+    }
+
+    /**
+     * @param uuid
+     */
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    /**
+     * @return email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return role
+     */
+    public String getRole() {
+        return role;
+    }
+
+    /**
+     * @param role
+     */
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    /**
+     * @return avatar
+     */
+    public String getAvatar() {
+        return avatar;
+    }
+
+    /**
+     * @param avatar
+     */
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
 }
