36b879e333fd0d9fc9612692b98e5006f537fa6a
Tn_magnet <40493125+zhaojunyip@users.noreply.github.com>
2021-04-25 21:12:16 +0800

    add addItem

--- src/main/java/com/company/project/web/ItemController.java
+++ src/main/java/com/company/project/web/ItemController.java
@@ -1,18 +1,20 @@
 package com.company.project.web;
 
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.JSONPObject;
 import com.company.project.core.Result;
 import com.company.project.core.ResultGenerator;
 import com.company.project.model.Item;
 import com.company.project.service.ItemService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * Created by CodeGenerator on 2021/04/21.
@@ -53,5 +55,27 @@ public class ItemController {
         List<Item> list = itemService.findAll();
         PageInfo pageInfo = new PageInfo(list);
         return ResultGenerator.genSuccessResult(pageInfo);
+    }
+
+    @PostMapping("/addItem")
+    public Result addItem(@RequestParam Map<String, String> params, HttpServletRequest request) {
+        String name = params.get("name");
+        String owner = params.get("owner");
+        int remain = Integer.valueOf(params.get("remain"));
+        boolean onsale = Boolean.parseBoolean(params.get("onsale"));
+        String description = params.get("description");
+        Item item = new Item();
+        item.setName(name);
+        item.setOwner(owner);
+        item.setDescription(description);
+        item.setOnsale(onsale);
+        item.setRemain(remain);
+        item.setUuid(UUID.randomUUID().toString());
+        try {
+            itemService.save(item);
+        } catch (Exception e) {
+            return ResultGenerator.genFailResult("failed");
+        }
+        return ResultGenerator.genSuccessResult("success");
     }
 }
