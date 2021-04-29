9dcaeb114c43e64f68e669cc6f9dddbcf2199128
Vergil <458361036@qq.com>
2021-04-29 15:47:11 +0800

    add createOrder,setBill,setFinish

--- src/main/java/com/company/project/web/OrderController.java
+++ src/main/java/com/company/project/web/OrderController.java
@@ -1,18 +1,28 @@
 package com.company.project.web;
 
+import com.alibaba.fastjson.JSON;
 import com.company.project.core.Result;
 import com.company.project.core.ResultGenerator;
+import com.company.project.model.Item;
 import com.company.project.model.Order;
+import com.company.project.model.User;
 import com.company.project.service.OrderService;
+import com.company.project.service.UserService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.sql.Time;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by CodeGenerator on 2021/04/21.
@@ -53,5 +63,53 @@ public class OrderController {
         List<Order> list = orderService.findAll();
         PageInfo pageInfo = new PageInfo(list);
         return ResultGenerator.genSuccessResult(pageInfo);
+    }
+
+    @PostMapping("/createOrder")
+    public Result createOrder(@RequestParam Map<String,String> params){
+        String itemData=params.get("itemData");
+        List<Item> items=JSON.parseArray(itemData,Item.class);
+        String buyer=params.get("buyer");
+        String delivery=params.get("delivery");
+        Date time=new Date();
+        int paid=Integer.parseInt(params.get("paid"));
+        //TODO:finish项的类型需要确定
+        String comment=params.get("comment");
+
+        Order order=new Order();
+        order.setItems(JSON.toJSONString(items));
+        order.setBuyer(buyer);
+        order.setDelivery(delivery);
+        order.setTime(time);
+        order.setPaid(false);
+        order.setFinish(false);
+        order.setComment(comment);
+        try {
+            orderService.save(order);
+        } catch (Exception e) {
+            return ResultGenerator.genFailResult("failed");
+        }
+        return ResultGenerator.genSuccessResult("success");
+    }
+
+    @PostMapping("/setBill")
+    public Result setBill(@RequestParam String orderUUID,String billUUID){
+        Order order=orderService.findById(orderUUID);
+        order.setBill(billUUID);
+        order.setPaid(true);
+        return ResultGenerator.genSuccessResult("success");
+    }
+
+    @PostMapping("/setFinish")
+    public Result setFinish(@RequestParam String orderUUID){
+        Order order=orderService.findById(orderUUID);
+        order.setFinish(true);
+        return ResultGenerator.genSuccessResult("success");
+    }
+
+    @PostMapping("/listOrderByUUID")
+    public Result listOrderByUUID(@RequestParam String UUID){
+
+        return ResultGenerator.genSuccessResult("succeess");
     }
 }
