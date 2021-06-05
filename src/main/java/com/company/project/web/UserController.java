7b0fca36bf00a9125496f3dee6cda2e7263355a8
Regressionbug <1421113602@qq.com>
2021-06-05 17:42:14 +0800

    登录接口增加了判断是否登录的操作，若已经登录，则会返回return ResultGenerator.genFailResult("已经登录了");进行相应输出

--- src/main/java/com/company/project/web/UserController.java
+++ src/main/java/com/company/project/web/UserController.java
@@ -45,7 +45,7 @@ public class UserController {
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
-     {
+    {
         auth=new Auth(stringRedisTemplate);
     }
     @PostMapping("/add")
