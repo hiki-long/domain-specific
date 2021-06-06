c443b3189b74931fbe14bedde706c89905d1fef5
hiki-long <411046642@qq.com>
2021-06-06 19:03:15 +0800

    添加addOrder接口正确实现
    Change-Id: I906b7b24936f8885ca82e0d5028148e353e1006b

--- src/main/java/com/company/project/web/OrderController.java
+++ src/main/java/com/company/project/web/OrderController.java
@@ -1,11 +1,14 @@
 package com.company.project.web;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.company.project.core.Result;
 import com.company.project.core.ResultGenerator;
 import com.company.project.model.Item;
 import com.company.project.model.Order;
 import com.company.project.model.User;
+import com.company.project.service.ItemService;
 import com.company.project.service.OrderService;
 import com.company.project.service.UserService;
 import com.github.pagehelper.PageHelper;
@@ -20,6 +23,7 @@ import java.sql.Struct;
 import java.sql.Time;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -32,11 +36,14 @@ import java.util.Map;
 public class OrderController {
     @Resource
     private OrderService orderService;
+    private ItemService itemService;
 
     private class ItemNumber {
         String itemUUID;
         Integer number;
-        float price;
+        String owner;
+        String buyer;
+        double price;
     }
 
     @PostMapping("/add")
@@ -73,29 +80,37 @@ public class OrderController {
 
     @CrossOrigin
     @PostMapping("/createOrder")
-    public Result createOrder(@RequestParam Map<String, String> params) {
-        String itemData = params.get("itemData");
-        List<Item> items = JSON.parseArray(itemData, Item.class);
-        String buyer = params.get("buyer");
-        String delivery = params.get("delivery");
+    public Result createOrder(@RequestParam String orderlist) {
+//    public Result createOrder(@RequestBody ArrayList<String> params) {
+        JSONArray json = JSONObject.parseArray(orderlist);
+        System.out.println(json.get(1));
+        /*List<ItemNumber> items=null;
+        for(String itemData:itemDatas){
+            JSONArray jsonArray = JSONObject.parseArray(itemData);
+            JSONObject jsonObject=(JSONObject) jsonArray.get(0);
+            Map<String,Object> map = jsonObject.getInnerMap();
+            map.get("itemUUID");
+
+        }
+        List<String> sellers=null;
         Date time = new Date();
-        int paid = Integer.parseInt(params.get("paid"));
-        //TODO:finish项的类型需要确定
-        String comment = params.get("comment");
 
         Order order = new Order();
         order.setItems(JSON.toJSONString(items));
-        order.setBuyer(buyer);
-        order.setDelivery(delivery);
+        for(ItemNumber item :items){
+            sellers.add(item.owner);
+            item.price=itemService.findById(item.itemUUID).getPrice();
+        }
+        order.setBuyer(items.get(0).buyer);
+        order.setDelivery("暂无数据");
         order.setTime(time);
-        order.setPaid(false);
+        order.setPaid(true);
         order.setFinish(false);
-        order.setComment(comment);
         try {
             orderService.save(order);
         } catch (Exception e) {
             return ResultGenerator.genFailResult("failed");
-        }
+        }*/
         return ResultGenerator.genSuccessResult("success");
     }
 
