f7215e39301d91be76ac4e9e4bd1b13c01df03d7
Regressionbug <1421113602@qq.com>
2021-09-02 11:35:52 +0800

    修改对应表的mapper的部分

--- src/main/java/com/company/project/model/Item.java
+++ src/main/java/com/company/project/model/Item.java
@@ -24,6 +24,16 @@ public class Item {
 
     private String image;
 
+    private Integer version;
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
     /**
      * @return UUID
      */
